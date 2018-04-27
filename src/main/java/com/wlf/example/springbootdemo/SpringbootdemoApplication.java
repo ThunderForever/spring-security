ebb01090a896f320fca464258715a97373e8ba30
DESKTOP-DCL8KK7\王力帆 <1710129303@qq.com>
2018-04-27 14:14:21 +0800

    通过在application.properties加入username和password来实现登录问题

---
+++ src/main/java/com/wlf/example/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,111 @@
+package com.wlf.example.springbootdemo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+@RestController
+@SpringBootApplication
+@EnableWebSecurity
+public class SpringbootdemoApplication{
+    public static void main(String[] args){
+        SpringApplication.run(
+                SpringbootdemoApplication.class,args
+        );
+    }
+
+    @RequestMapping("/security")
+    public String security(){
+        return "hello world security";
+    }
+
+    @RequestMapping("/wordladder")
+
+
+
+
+    public String wordladder(String firstword,String target) throws FileNotFoundException {
+        wordladder englishwords = new wordladder();
+        englishwords.Read();                                        //English dictionary
+        Queue<Stack<String>> queue = new LinkedList<Stack<String>>(); //store the possible word ladder
+
+
+
+        String s1 = firstword;
+
+        if ((!englishwords.search(s1))) {
+            return("Maybe your first word is not in the dictionary.");
+        }
+        englishwords.dictionary.add(s1);                            //add first word to the dictionary
+
+
+        String s2 = target;
+
+        if ((!(englishwords.search(s2)))) {
+            return("Maybe your target is not in the dictionary.");
+        }
+
+
+
+        int len1 = s1.length();
+        int len2 = s2.length();
+        englishwords.dictionary.add(s2);                            //add target to the dictionary
+        if (len1 != len2) {
+            return("The length of \"" + s1 + "\" is not equal to the length of \"" + s2 + "\".");
+        } else {
+            if (s1 != s2) {
+                Stack<String> stack = new Stack<String>();
+                stack.push(s1);
+                queue.offer(stack);
+
+                Loop:
+                while (!queue.isEmpty()) {
+                    Stack<String> localstack = new Stack<String>();
+                    localstack = queue.poll();
+                    s1 = localstack.peek();
+                    for (int j = 0; j < len1; j++) {
+                        for (int i = 'a'; i <= 'z'; i++) {
+                            String newString = null;
+                            newString = wordladder.change(s1, j, len1, (char) i);
+                            if (newString.equals(s2)) {
+                                localstack.push(newString);
+                                stack = localstack;
+                                break Loop;
+                            } else if (englishwords.search(newString) && (!newString.equals(s1))) {
+                                Stack<String> sclone = (Stack<String>) localstack.clone();
+                                sclone.push(newString);
+                                queue.offer(sclone);
+                            }
+                        }
+                    }
+                }
+                if (stack.size() != 1) {
+                    String ladder=stack.pop()+"\t";
+                    int i=1;
+                    while(!stack.isEmpty()){
+                        ladder+=(stack.pop()+"\t");
+                        i++;
+                    }
+                    return(ladder+"\n"+"The length between "+firstword+" and "+target+" is " + i);
+
+                } else {
+                    return("I'm sorry. The word ladder isn't exist");
+                }
+            } else {
+                return("The two words are the same!");
+            }
+
+        }
+
+
+    }
+}
